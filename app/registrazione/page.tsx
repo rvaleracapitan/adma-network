19cb2f6403d7daf4048897e9cc059654717d90fe
rvaleracapitan <renato.valera.capitan@gmail.com>
2026-08-21 08:19:18 +0000

    ripristino chiave Turnstile reale

--- app/registrazione/page.tsx
+++ app/registrazione/page.tsx
@@ -233,7 +233,7 @@ export default function Registrazione() {
               {sending ? 'Invio in corso...' : 'INVIA RICHIESTA DI REGISTRAZIONE'}
             </button>
             <Turnstile
-              siteKey="1x00000000000000000000AA"
+              siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY!}
               onSuccess={(token) => setCaptchaToken(token)}
               onExpire={() => setCaptchaToken(null)}
               style={{ marginBottom: '1rem' }}
